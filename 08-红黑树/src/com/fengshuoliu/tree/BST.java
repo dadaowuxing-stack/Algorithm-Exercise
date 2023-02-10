d10615f5deb673f171d32e3391d77746c917d807
fengshuo.liu <fsliu136@gmail.com>
2023-02-10 14:53:43 +0800

    红黑树删除

--- 08-红黑树/src/com/fengshuoliu/tree/BST.java
+++ 08-红黑树/src/com/fengshuoliu/tree/BST.java
@@ -82,10 +82,9 @@ public class BST<E> extends BinaryTree<E> {
 
     /**
      * 删除 node 之后的调整
-     *
-     * @param node 删除的节点
+     * @param node 被删除的节点  或者 用以取代被删除节点的子节点(当被删除节点的度为1)
      */
-    protected void afterRemove(Node<E> node, Node<E> replacement) {
+    protected void afterRemove(Node<E> node) {
     }
 
     /**
@@ -157,11 +156,11 @@ public class BST<E> extends BinaryTree<E> {
                 node.parent.right = replacement;
             }
             // 删除节点之后的处理
-            afterRemove(node, replacement);
+            afterRemove(replacement);
         } else if (node.parent == null) { // node是叶子节点并且是根节点
             root = null;
             // 删除节点之后的处理
-            afterRemove(node, null);
+            afterRemove(node);
         } else { // node是叶子节点,但不是根节点
             if (node == node.parent.left) {
                 node.parent.left = null;
@@ -169,7 +168,7 @@ public class BST<E> extends BinaryTree<E> {
                 node.parent.right = null;
             }
             // 删除节点之后的处理
-            afterRemove(node, null);
+            afterRemove(node);
         }
 
     }
