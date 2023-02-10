657e26666a2b21fed5faf2576095c46ea1b326b2
fengshuo.liu <fsliu136@gmail.com>
2023-02-10 12:11:01 +0800

    红黑树删除节点

--- 08-红黑树/src/com/fengshuoliu/tree/RBTree.java
+++ 08-红黑树/src/com/fengshuoliu/tree/RBTree.java
@@ -23,9 +23,68 @@ public class RBTree<E> extends BBST<E> {
             black(replacement);
             return;
         }
-        // 删除的是黑色叶子节点
-        
-
+        Node<E> parent = node.parent;
+        // 删除的是根节点
+        if (parent == null) return;
+        // 删除的是黑色叶子节点[下溢]
+        // 判断被删除的node是左还是右
+        boolean left = parent.left == null;
+        Node<E> sibling = left ? parent.right : parent.left;
+        if (left) { // 被删除的节点在左边,兄弟节点在右边
+            if (isRed(sibling)) { // 兄弟节点是红色
+                black(sibling);
+                red(parent);
+                rotateLeft(parent);
+                sibling = parent.right;
+            }
+            // 兄弟节点必然是黑色
+            if (isBlack(sibling.left) && isBlack(sibling.right)) {
+                // 兄弟节点没有1个红色子节点,父节点要向下跟兄弟节点合并
+                boolean parentBlack = isBlack(parent);
+                black(parent);
+                red(sibling);
+                if (parentBlack) {
+                    afterRemove(parent, null);
+                }
+            } else { // 兄弟节点至少有1个红色节点,向兄弟节点借元素
+                if (isBlack(sibling.right)) { // 兄弟节点右边是黑色,兄弟要先旋转
+                    rotateLeft(sibling);
+                    // 更换兄弟
+                    sibling = parent.right;
+                }
+                color(sibling, colorOf(parent));
+                black(sibling.left);
+                black(parent);
+                rotateRight(parent);
+            }
+        } else { // 被删除的节点在右边,兄弟节点在左边
+            if (isRed(sibling)) { // 兄弟节点是红色
+                black(sibling);
+                red(parent);
+                rotateRight(parent);
+                sibling = parent.left;
+            }
+            // 兄弟节点必然是黑色
+            if (isBlack(sibling.left) && isBlack(sibling.right)) {
+                // 兄弟节点没有1个红色子节点,父节点要向下跟兄弟节点合并
+                boolean parentBlack = isBlack(parent);
+                black(parent);
+                red(sibling);
+                if (parentBlack) {
+                    afterRemove(parent, null);
+                }
+            } else { // 兄弟节点至少有1个红色节点,向兄弟节点借元素
+                if (isBlack(sibling.left)) { // 兄弟节点左边是黑色,兄弟要先旋转
+                    rotateLeft(sibling);
+                    // 更换兄弟
+                    sibling = parent.left;
+                }
+                color(sibling, colorOf(parent));
+                black(sibling.left);
+                black(parent);
+                rotateRight(parent);
+            }
+        }
     }
 
     @Override
