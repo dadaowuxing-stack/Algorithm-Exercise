90e75bff9c27eb0de40ac039b5b6031059c443ba
fengshuo.liu <fsliu136@gmail.com>
2023-03-08 15:57:17 +0800

    贪心

---
+++ 2-06-贪心/src/com/fengshuoliu/Main.java
@@ -0,0 +1,16 @@
+package com.fengshuoliu;
+
+/**
+ * 贪心(Greedy)
+ * ◼ 贪心策略，也称为贪婪策略
+ * 每一步都采取当前状态下最优的选择(局部最优解)，从而希望推导出全局最优解
+ * ◼ 贪心的应用
+ * 哈夫曼树
+ * 最小生成树算法:Prim、Kruskal
+ * 最短路径算法:Dijkstra
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
