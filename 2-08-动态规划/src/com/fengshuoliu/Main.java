1239a5d1ffdd2eae007681f0038ff34789ca3468
fengshuo.liu <fsliu136@gmail.com>
2023-03-09 17:35:34 +0800

    动态规划 1

---
+++ 2-08-动态规划/src/com/fengshuoliu/Main.java
@@ -0,0 +1,16 @@
+package com.fengshuoliu;
+
+/**
+ * 动态规划(Dynamic Programming)
+ * ◼ 动态规划，简称DP
+ * 是求解最优化问题的一种常用策略
+ * ◼ 通常的使用套路(一步一步优化)
+ * 1 暴力递归(自顶向下，出现了重叠子问题)
+ * 2 记忆化搜索(自顶向下)
+ * 3 递推(自底向上)
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
