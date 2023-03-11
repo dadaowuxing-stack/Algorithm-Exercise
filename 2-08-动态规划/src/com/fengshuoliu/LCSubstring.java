220fab7581f1d1f3a0d3b1cd38ada60dad5578ea
fengshuo.liu <fsliu136@gmail.com>
2023-03-11 15:19:53 +0800

    动态规划

--- 2-08-动态规划/src/com/fengshuoliu/LCSubstring.java
+++ 2-08-动态规划/src/com/fengshuoliu/LCSubstring.java
@@ -8,7 +8,35 @@ package com.fengshuoliu;
  */
 public class LCSubstring {
     public static void main(String[] args) {
-        System.out.println(lcs2("ABCBA","BABCA"));
+        System.out.println(lcs3("ABCBA","ABBA"));
+    }
+
+    static int lcs3(String str1, String str2) {
+        if (str1 == null || str2 == null) return 0;
+        char[] chars1 = str1.toCharArray();
+        if (chars1.length == 0) return 0;
+        char[] chars2 = str2.toCharArray();
+        if (chars2.length == 0) return 0;
+
+        char[] rowChars = chars1, colsChars = chars2;
+        if (chars1.length < chars2.length) {
+            colsChars = chars1;
+            rowChars = chars2;
+        }
+
+        int[] dp = new int[colsChars.length + 1];
+        int max = 0;
+        for (int row = 1; row <= rowChars.length; row++) {
+            for (int col = colsChars.length; col >= 1; col--) {
+                if (chars1[row - 1] != chars2[col - 1]) {
+                    dp[col] = 0;
+                } else {
+                    dp[col] = dp[col - 1] + 1;
+                    max = Math.max(dp[col], max);
+                }
+            }
+        }
+        return max;
     }
 
     static int lcs2(String str1, String str2) {
